5af1280e387f9d9396885a2749b80cab98218485
Marcos <marcosespeleta05@gmail.com>
2025-12-27 20:03:42 +0100

    Initial commit: Estructura del proyecto e inicialización correcta del backend

---
+++ backend/src/main/java/com/marcos/AppInternaReservasApplication.java
@@ -0,0 +1,13 @@
+package com.marcos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppInternaReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppInternaReservasApplication.class, args);
+	}
+
+}
